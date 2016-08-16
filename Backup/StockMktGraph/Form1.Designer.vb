7a518e8479ec7dc8fe22aeb7664d8ebfd83e9582
nkgautam <narendra.gautam1@gmail.com>
2016-08-16 11:36:04 +0530

    Initial Commit

---
+++ Backup/StockMktGraph/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.AccessibleDescription = Nothing
+        Me.PictureBox1.AccessibleName = Nothing
+        resources.ApplyResources(Me.PictureBox1, "PictureBox1")
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.PictureBox1.BackgroundImage = Nothing
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Font = Nothing
+        Me.PictureBox1.ImageLocation = Nothing
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AccessibleDescription = Nothing
+        Me.Label1.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Font = Nothing
+        Me.Label1.Name = "Label1"
+        '
+        'Label3
+        '
+        Me.Label3.AccessibleDescription = Nothing
+        Me.Label3.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Font = Nothing
+        Me.Label3.Name = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AccessibleDescription = Nothing
+        Me.Label4.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Font = Nothing
+        Me.Label4.Name = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.AccessibleDescription = Nothing
+        Me.Label5.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.Font = Nothing
+        Me.Label5.Name = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.AccessibleDescription = Nothing
+        Me.Label6.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.Label6.Font = Nothing
+        Me.Label6.Name = "Label6"
+        '
+        'Label2
+        '
+        Me.Label2.AccessibleDescription = Nothing
+        Me.Label2.AccessibleName = Nothing
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Font = Nothing
+        Me.Label2.Name = "Label2"
+        '
+        'Form1
+        '
+        Me.AccessibleDescription = Nothing
+        Me.AccessibleName = Nothing
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Nothing
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = Nothing
+        Me.Icon = Nothing
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Backup/StockMktGraph/Form1.vb
@@ -0,0 +1,160 @@
+﻿Imports System.Threading
+
+Public Class Form1
+
+    Dim x_mm, y_mm As Int16     'cordinats of the line
+    Dim myPen = New Pen(Color.Blue) 'graph pen color
+    Dim gridPen = New Pen(Color.LightGray) 'grid pen color
+    Dim graphics As Graphics
+
+    Structure stockData
+        Dim sdate As Date
+        Dim open As Single
+        Dim high As Single
+        Dim low As Single
+        Dim close As Single
+        Dim vol As Integer
+    End Structure
+    Structure sdSize
+        Dim symbol As String        ' the stock symbol
+        Dim n As Integer            ' the # of elements in the stockData array that is
+        '                           '   returned to the calling program by getAH
+    End Structure
+    Public Sub getAH(ByRef sd() As stockData, ByRef sd1 As sdSize)
+        Dim inpfilenum As Integer, inpfilename As String
+        Dim filebase As String
+        Dim stg As String
+        Dim k As Integer
+        Dim alocal() As String
+        Dim localArray(300) As stockData
+        '
+        ' NOTE TO CODER: for your testing, you can set your own filebase and filename
+        '                what I have here is what works for me
+        '
+        'filebase = "D:\VisualStudioProjects\StockMktGraph\StockMktGraph\"
+        filebase = "..\"
+        inpfilenum = FreeFile()
+        inpfilename = filebase & "AH total.txt"
+        FileOpen(inpfilenum, inpfilename, OpenMode.Input, OpenAccess.Read)
+        '
+        ' read the first line and get the symbol and size
+        '
+        stg = LineInput(inpfilenum)
+        alocal = Split(stg, ",")
+        sd1.symbol = alocal(0)
+        sd1.n = CInt(alocal(1))
+        '
+        ' read the rest of the lines and put the data into sd
+        '
+        Thread.CurrentThread.CurrentCulture = New Globalization.CultureInfo("en-US")
+        For ix = 0 To sd1.n - 1
+            '
+            ' get a line from the file and split it into the various parts
+            '
+            stg = LineInput(inpfilenum)
+            alocal = Split(stg, ",")
+            '
+            ' convert the date string (which is in a format of MY preference) into a date variable
+            '
+            stg = Mid(alocal(0), 5, 2) & "/" & Mid(alocal(0), 7, 2) & "/" & Microsoft.VisualBasic.Left(alocal(0), 4)
+            '
+            ' get the rest of this line into sd
+            '
+
+            sd(ix).sdate = CDate(stg)
+            sd(ix).open = CSng(alocal(1))
+            sd(ix).high = CSng(alocal(2))
+            sd(ix).low = CSng(alocal(3))
+            sd(ix).close = CSng(alocal(4))
+            sd(ix).vol = CInt(alocal(5))
+        Next
+        FileClose(inpfilenum)
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseMove
+
+        'Copy mouse move position
+        x_mm = e.X
+        y_mm = e.Y
+
+        'Init PictureBox
+        Dim bmp01 As New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        PictureBox1.Image = bmp01
+        GC.SuppressFinalize(bmp01)
+        GC.Collect()
+
+        'create array of stockData
+        Dim sd() As stockData = New stockData(300) {}
+        Dim sd1 As sdSize
+
+        'call getAH for sd updation
+        getAH(sd, sd1)
+
+        'Graphics object initialization
+        Dim bmp As New Bitmap(PictureBox1.Image)
+        graphics = graphics.FromImage(bmp)
+        PictureBox1.Image = bmp
+
+        'x,y coordinates of graph
+        Dim x As Single, y As Single
+
+        'erase lines
+        graphics.Clear(Color.White)
+
+        'initial x,y position
+        x = 0
+        y = 0
+        'interval for scaling
+        Dim interval As Single
+
+        'Vertical grid drawing
+        For cnt = 1 To 25
+            graphics.DrawLine(gridPen, 0, cnt * 25, 1008, cnt * 25)
+        Next
+
+        'Horigontal grid drawing
+        For cnt = 1 To 12
+            graphics.DrawLine(gridPen, cnt * 84, 0, cnt * 84, 500)
+        Next
+        For ix = 0 To sd1.n - 1
+            'read sd().open values
+            x = sd(ix).open * 10
+            y = sd(ix + 1).open * 10
+
+            'Scaling calcution
+            x = CInt(x)
+            y = CInt(y)
+            x = 350 - x
+            y = 350 - y
+            x = x + (x * 0.7)
+            y = y + (y * 0.7)
+            x = x / 0.7
+            y = y / 0.7
+
+            'draw graph
+            graphics.DrawLine(myPen, interval, x, interval + 4, y)
+            interval = interval + 4
+        Next
+
+        Dim myPen1 = New Pen(Color.Red)
+
+        'Draw red line cursor
+        graphics.DrawLine(myPen1, x_mm, 0, x_mm, 500)
+
+        'Update lables with sd() data
+        Label1.Text = "Stock Symbol Date = " & sd(x_mm / 4).sdate
+        Label2.Text = "Open = " & sd(x_mm / 4).open
+        Label3.Text = "High = " & sd(x_mm / 4).high
+        Label4.Text = "Low =  " & sd(x_mm / 4).low
+        Label5.Text = "Close =  " & sd(x_mm / 4).close
+        Label6.Text = "Volume = " & sd(x_mm / 4).vol
+        Refresh()
+        '
+        'Release resources
+        GC.Collect()
+    End Sub
+End Class
